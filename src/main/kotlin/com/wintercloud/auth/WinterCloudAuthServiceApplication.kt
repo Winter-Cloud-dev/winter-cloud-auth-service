d5c0c13ff16b0843e0758630d5980037f7e5e022
Tae Hee Jang <kancth03@gmail.com>
2025-08-15 22:02:49 +0900

    refactor: package name 변경

--- src/main/kotlin/com/wintercloud/auth/WinterCloudAuthServiceApplication.kt
+++ src/main/kotlin/com/wintercloud/auth/WinterCloudAuthServiceApplication.kt
@@ -1,4 +1,4 @@
-package com.wintercloud.authservice
+package com.wintercloud.auth
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
